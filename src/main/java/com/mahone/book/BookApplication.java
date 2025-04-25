88e714aabedc4158956b308200852c978a9658ac
mahone <3282564613@qq.com>
2025-04-25 20:15:34 +0800

    thirdCommit

--- src/main/java/com/mahone/book/BookApplication.java
+++ src/main/java/com/mahone/book/BookApplication.java
@@ -11,6 +11,7 @@ public class BookApplication {
     public static void main(String[] args) {
         SpringApplication.run(BookApplication.class, args);
         System.out.println("hello world");
+        System.out.println("hello git3");
     }
 
 }
